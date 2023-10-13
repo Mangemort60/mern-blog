98383f9cce02d797de751a5717bd1de51bfd1c39
Hafid HADDAOUI <hahaddaoui@gmail.com>
2023-10-13 16:42:36 +0200

    added confirmation for postEditor and icon website

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Register from './components/Register';
 import UserProfile from './components/UserProfile/UserProfile';
 import { Routes, Route } from 'react-router-dom';
 import axios from 'axios';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext, User } from './contexts/UserContext';
 import { useCookies } from 'react-cookie';
 import PostEditor from './components/Post/PostEditor';
@@ -45,6 +45,8 @@ function App() {
   const [cookies] = useCookies(['userId', 'token']);
   const { user, setUser } = useContext(UserContext);
   const { setIsUserLoading } = useContext(UserContext);
+  const [confirmationPostMessage, setConfirmationPostMessage] = useState('');
+
   console.log(process.env.NODE_ENV);
 
   useEffect(() => {
@@ -80,6 +82,24 @@ function App() {
   return (
     <>
       <Navbar />
+      {confirmationPostMessage && (
+        <div
+          className="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 mr-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Info</span>
+          <div>{confirmationPostMessage}</div>
+        </div>
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -87,7 +107,14 @@ function App() {
         <Route path="/post/:id" element={<Post />} />
         <Route element={<ProtectedAdminRoute user={user} />}>
           <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/post/editor" element={<PostEditor />} />
+          <Route
+            path="/post/editor"
+            element={
+              <PostEditor
+                setConfirmationPostMessage={setConfirmationPostMessage}
+              />
+            }
+          />
           <Route path="/post/update/:id" element={<UpdatePost />} />
         </Route>
         <Route element={<ProtectedUserRoute />}>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useCookies } from 'react-cookie';
 import { Link, useNavigate } from 'react-router-dom';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../contexts/UserContext';
 import defaultHeadshot from '../assets/defaultHeadshot.webp';
 
@@ -21,9 +21,9 @@ const NavbarMenu = () => {
 
   return (
     <>
-      <Navbar fluid rounded>
-        <Navbar.Brand href="https://flowbite-react.com">
-          <span className="self-center whitespace-nowrap text-5xl font-dancing dark:text-white">
+      <Navbar fluid rounded className="">
+        <Navbar.Brand href="https://youssra-therapie.fr/">
+          <span className="self-center whitespace-nowrap text-4xl  sm:text-5xl font-dancing dark:text-white">
             Youssra Thérapie
           </span>
         </Navbar.Brand>
@@ -50,25 +50,32 @@ const NavbarMenu = () => {
               <Dropdown.Item>
                 <Link to={'/user-profile'}>Mon profil</Link>
               </Dropdown.Item>
-              <Dropdown.Item>
-                {cookies.userId && cookies.token && user.isAdmin && (
+
+              {cookies.userId && cookies.token && user.isAdmin && (
+                <Dropdown.Item>
                   <Link to={'/admin'}>Admin</Link>
-                )}
-              </Dropdown.Item>
+                </Dropdown.Item>
+              )}
+
               <Dropdown.Divider />
               <Dropdown.Item onClick={() => HandleLogout()}>
                 Se deconnecter
               </Dropdown.Item>
             </Dropdown>
           ) : (
-            <Link to={'/login'}>Login</Link>
+            <Link
+              to={'/login'}
+              className="mt-2 md:m-0 border-black border-2 px-2 py-1 ml-4"
+            >
+              Login
+            </Link>
           )}
 
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
-          <Navbar.Link href="#" active>
-            Accueil
+          <Navbar.Link>
+            <Link to={'/'}>Accueil</Link>
           </Navbar.Link>
           <Navbar.Link>
             <Link to={'/articles'}>Articles</Link>
@@ -80,12 +87,16 @@ const NavbarMenu = () => {
             <Link to={'/offre'}>Offres</Link>
           </Navbar.Link>
           <Navbar.Link href="#">
-            <Link to={'/calendly'} className="bg-[#afa4ce] text-white w-20">
+            <Link
+              to={'/calendly'}
+              className="bg-[#afa4ce] text-white w-20 py-1 px-2 rounded-sm"
+            >
               Prendre RDV
             </Link>
           </Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
+      <hr className="w-1/2 mx-auto mt-8" />
     </>
   );
 };
